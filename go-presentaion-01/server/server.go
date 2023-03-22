266afaa56f89a67e384270bc891e070cbfec16f3
romel.campbell <romel.campbell@regeneron.com>
2023-03-22 11:51:13 -0400

    http code

---
+++ go-presentaion-01/server/server.go
@@ -0,0 +1,16 @@
+// Serving http://localhost:8080/world
+package main
+
+import (
+	"fmt"
+	"http"
+)
+
+func handler(c *http.Conn, r *http.Request) {
+	fmt.Fprintf(c, "Hello, %s.", r.URL.Path[1:])
+}
+
+func main() {
+	http.ListenAndServe(":8080",
+		http.HandlerFunc(handler))
+}
