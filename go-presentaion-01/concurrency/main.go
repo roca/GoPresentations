84cf51eefb7bf2caa63cc67da684af537f541450
Romel Campbell <RomelCampbell@gmail.com>
2023-03-25 18:10:38 -0400

    closure aroung wg

--- go-presentaion-01/concurrency/main.go
+++ go-presentaion-01/concurrency/main.go
@@ -10,15 +10,15 @@ func main() {
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
+			defer wg.Done()
 			fmt.Println(i)
-			wg.Done()
 		}(i)
 	}
 	for i := 'a'; i < 'e'; i++ {
 		wg.Add(1)
 		go func(i rune) {
-			fmt.Printf("%c\n", i)
 			defer wg.Done()
+			fmt.Printf("%c\n", i)
 		}(i)
 	}
 	wg.Wait()
