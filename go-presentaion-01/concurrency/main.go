d21009c4f5a27c49bbdfba535f7bc5b313ec68af
romel.campbell <romel.campbell@regeneron.com>
2023-04-20 11:42:27 -0400

    WIP

--- go-presentaion-01/concurrency/main.go
+++ go-presentaion-01/concurrency/main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
+	"runtime"
 	"sync"
+	"time"
 )
 
+// START OMIT
 func main() {
+	runtime.GOMAXPROCS(4)
 	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
 		wg.Add(1)
 		go func(i int) {
 			defer wg.Done()
+			time.Sleep(time.Second * 2)
 			fmt.Println(i)
 		}(i)
 	}
@@ -18,8 +23,11 @@ func main() {
 		wg.Add(1)
 		go func(i rune) {
 			defer wg.Done()
+			time.Sleep(time.Second * 2)
 			fmt.Printf("%c\n", i)
 		}(i)
 	}
 	wg.Wait()
 }
+
+//END OMIT
